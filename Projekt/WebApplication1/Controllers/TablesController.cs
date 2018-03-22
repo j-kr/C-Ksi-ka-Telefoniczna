8b28ba5b49f256d1a27fc5502b48370a1d906123
J.K <jakub.kruczkowski@o2.pl>
2018-03-22 12:51:07 +0100

    Initial commit

---
+++ Projekt/WebApplication1/Controllers/TablesController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1;
+
+namespace WebApplication1.Controllers
+{
+    public class TablesController : Controller
+    {
+        private Database1Entities db = new Database1Entities();
+
+        // GET: Tables
+
+        public ActionResult Index(string szukana)
+        {
+            var ks_tel = from s in db.Table
+                         select s;
+            if (!String.IsNullOrEmpty(szukana))
+            {
+                ks_tel = ks_tel.Where(c => c.Imie.Contains(szukana) || c.Naziwsko.Contains(szukana) || c.Miejscowosc.Contains(szukana) || c.Nr_domu.Contains(szukana) || c.Ulica.Contains(szukana) || c.Telefon.Contains(szukana));
+            }
+            return View(ks_tel);
+        }
+
+
+
+
+
+
+
+        // GET: Tables/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table table = db.Table.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+
+ 
+
+
+
+
+
+    // GET: Tables/Create
+    public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tables/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Imie,Naziwsko,Miejscowosc,Ulica,Nr_domu,Telefon")] Table table)
+        {
+
+
+            if (db.Table.Any(model => model.Telefon == table.Telefon))
+            {
+                return View("Error");
+            }
+
+            if (ModelState.IsValid)
+            {
+                db.Table.Add(table);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(table);
+        }
+
+        // GET: Tables/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table table = db.Table.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+        // POST: Tables/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Imie,Naziwsko,Miejscowosc,Ulica,Nr_domu,Telefon")] Table table)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(table).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(table);
+        }
+
+        // GET: Tables/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table table = db.Table.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+        // POST: Tables/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Table table = db.Table.Find(id);
+            db.Table.Remove(table);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+//PL
+Projekt ksi¹¿ki telefonicznej z wykorzystaniem entity framework.
+
+//ENG
+A phone book project with help of entity framework.
